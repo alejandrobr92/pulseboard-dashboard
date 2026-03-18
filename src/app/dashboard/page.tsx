80dcfd06a097f10278834319664f01aab5789735
alejandrobr92 <reyesbautista.contact@gmail.com>
2026-03-17 21:05:00 -0600

    Day 1 setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import MainLayout from "@/components/layout/mainLayout";
+
+export default function Dashboard() {
+    return (
+        <MainLayout>
+            <div>Dashboard</div>
+        </MainLayout>
+    );
+}
---
+++ src/components/layout/mainLayout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import Sidebar from "./sidebar";
+import Topbar from "./topbar";
+
+export default function MainLayout({ children }: Readonly<{ children: ReactNode }>): ReactNode {
+    return (<div className="flex h-screen">
+        <Sidebar />
+        <div className="flex flex-col flex-1">
+            <Topbar />
+            <main className="p-6">{children}</main>
+        </div>
+    </div>)
+};
